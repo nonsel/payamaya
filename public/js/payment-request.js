8f2069049c899ef093fa2461267bf7bb5a6d94e4
Nonsel <nonselsomera1@gmail.com>
2020-09-06 23:05:56 +0800

    JS disable button after click

--- public/js/payment-request.js
+++ public/js/payment-request.js
@@ -1,17 +1,20 @@
 $("form").submit(function(e){
-  console.log($("#PaymentForm").serialize())
+  // console.log($("#PaymentForm").serialize())
   e.preventDefault();
+  $(".btn-submit").prop("disabled",true);
   var base_url = 'http://localhost:8080/'
   $.ajax({
     url: base_url + 'Paymaya/request_payment',
     type: "POST",
-    // dataType: 'json',
+    dataType: 'json',
     data: $("#PaymentForm").serialize(),
     success: function(response){
-         window.location.href = response.redirectUrl;
+      window.location.href = response.redirectUrl;
+      $(".btn-submit").prop("disabled",false);
     },
     error: function(){
 
     }
-  });
-});
+  });//END:: AJAX
+
+});//END:: FORM SUBMIT
