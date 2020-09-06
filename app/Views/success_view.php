b25bcec8446db913832b4ec0a152330c42e95932
Nonsel <nonselsomera1@gmail.com>
2020-09-06 23:06:26 +0800

    add success page

---
+++ app/Views/success_view.php
@@ -0,0 +1,48 @@
+<html lang="en"><head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="<?= base_url('image/favicon.png'); ?>">
+
+    <title>Paymaya</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/checkout/">
+
+    <!-- Bootstrap core CSS -->
+    <link href="https://getbootstrap.com/docs/4.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="https://getbootstrap.com/docs/4.0/examples/checkout/form-validation.css" rel="stylesheet">
+  </head>
+
+  <body class="bg-light">
+
+    <div class="container">
+      <div class="py-5 text-center">
+        <img style="width:280px; height:100px;" class="d-block mx-auto mb-4" src="https://www.manilatimes.net/wp-content/uploads/2018/10/PayMaya-Logo20181007.jpg" alt="" width="72" height="72">
+        <!-- <h2>Checkout form</h2>	 -->
+      </div>
+
+      <div class="row d-flex justify-content-center">
+        <div class="col-md-8 order-md-1">
+          <center>
+            <img src="<?= base_url('image/success.png') ?>">
+            <h1>Payment Succesfully</h1>
+          </center>
+        </div>
+      </div>
+    </div>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+    <script src="https://getbootstrap.com/docs/4.0/assets/js/vendor/popper.min.js"></script>
+    <script src="https://getbootstrap.com/docs/4.0/dist/js/bootstrap.min.js"></script>
+    <script src="https://getbootstrap.com/docs/4.0/assets/js/vendor/holder.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="js/payment-request.js"></script>
+
+</body></html>
