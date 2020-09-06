374c305ee1d37d045c7423e35944c9092265bdfe
Nonsel <nonselsomera1@gmail.com>
2020-09-07 01:37:35 +0800

    remove payment-request.js and remove baseurl

--- app/Views/success_view.php
+++ app/Views/success_view.php
@@ -43,6 +43,5 @@
     <script src="https://getbootstrap.com/docs/4.0/dist/js/bootstrap.min.js"></script>
     <script src="https://getbootstrap.com/docs/4.0/assets/js/vendor/holder.min.js"></script>
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="js/payment-request.js"></script>
 
 </body></html>
