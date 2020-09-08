faca7a5f5bebe594297a74ac232eb7e375219277
Jc <johncasem.catalonia@myoffice.ph>
2020-09-09 02:35:48 +0800

    update payment-request resources

--- public/js/payment-request.js
+++ public/js/payment-request.js
@@ -19,3 +19,14 @@ function generateLink(valid){
   });//END:: AJAX
 
 }//END:: generateLink
+
+
+function selected_payment(link){
+  window.location.href = link;
+}
+
+$("#phone-number").keypress(function (e) {
+  if (e.which != 8 && e.which != 0 && (e.which < 48 || e.which > 57)) {
+    return false;
+  }
+});
