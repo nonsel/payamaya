a3d3603e82757f2125aec4c3bb5eb17d39ce6bb7
Jc <johncasem.catalonia@myoffice.ph>
2020-09-15 17:21:32 +0800

    update policy content

--- app/Views/privacy-policy.php
+++ app/Views/privacy-policy.php
@@ -1,1 +1,141 @@
-Privacy policy
+<html lang="en"><head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <meta name="robots" content="noindex" />
+    <link rel="icon" href="<?= base_url('image/logo-lindenteak.png'); ?>">
+    <title>Linden Teak Furniture | Privacy Policy</title>
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/checkout/">
+    <link href="https://getbootstrap.com/docs/4.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://getbootstrap.com/docs/4.0/examples/checkout/form-validation.css" rel="stylesheet">
+  </head>
+
+  <body class="">
+
+    <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 bg-white border-bottom shadow-sm">
+      <div class="container">
+        <a href="<?= base_url('/'); ?>">
+          <h5 class="my-0 mr-md-auto font-weight-normal"><img src="<?= base_url('image/logo-lindenteak.png'); ?>" class="img-fluid" alt="Responsive image"></h5>
+        </a>
+        <nav class="my-2 my-md-0 mr-md-3">
+        </nav>
+      </div>
+    </div>
+
+    <div class="container-fluid pt-5 pb-5" style="background: #f3f3f3; line-height: 2">
+      <div class="container">
+        <section class="mb-5">
+          <h2 class="mb-3">Linden Teak Furniture Privacy Policy</h2>
+          <p>This Privacy Policy describes how your personal information is collected, used, and shared when you visit or make a purchase from <a href="https://www.lindenteakfurniture.com/" target="_blank">https://www.lindenteakfurniture.com/</a> (the “Site”).</p>
+        </section>
+
+        <section class="mb-5">
+          <h4 class="mb-3">PERSONAL INFORMATION WE COLLECT</h4>
+          <p>When you visit the Site, we automatically collect certain information about your device, including information about your web browser, IP address, time zone, and some of the cookies that are installed on your device. Additionally, as you browse the Site, we collect information about the individual web pages or products that you view, what websites or search terms referred you to the Site, and information about how you interact with the Site. We refer to this automatically-collected information as “Device Information.”</p>
+          <br>
+          <p>We collect Device Information using the following technologies:</p>
+          <ul>
+            <li>“Cookies” are data files that are placed on your device or computer and often include an anonymous unique identifier. For more information about cookies, and how to disable cookies, visit <a href="http://www.allaboutcookies.org" target="_blank">http://www.allaboutcookies.org</a>.</li>
+            <li>“Log files” track actions occurring on the Site, and collect data including your IP address, browser type, Internet service provider, referring/exit pages, and date/time stamps.</li>
+            <li>“Web beacons,” “tags,” and “pixels” are electronic files used to record information about how you browse the Site.</li>
+          </ul>
+          <br>
+          <p>Additionally when you make a purchase or attempt to make a purchase through the Site, we collect certain information from you, including your name, billing address, shipping address, payment information (including credit card numbers, paymaya, gcash, unionbank), email address, and phone number.  We refer to this information as “Order Information.”</p>
+          <br>
+          <p>When we talk about “Personal Information” in this Privacy Policy, we are talking both about Device Information and Order Information.</p>
+        </section>
+
+        <section class="mb-5">
+          <h4 class="mb-3">HOW DO WE USE YOUR PERSONAL INFORMATION?</h4>
+          <p>We use the Order Information that we collect generally to fulfill any orders placed through the Site (including processing your payment information, arranging for shipping, and providing you with invoices and/or order confirmations).  Additionally, we use this Order Information to:</p>
+          <ul>
+            <li>Communicate with you;</li>
+            <li>Screen our orders for potential risk or fraud; and</li>
+            <li>When in line with the preferences you have shared with us, provide you with information or advertising relating to our products or services.</li>
+          </ul>
+          <br>
+          <p>
+            We use the Device Information that we collect to help us screen for potential risk and fraud (in particular, your IP address), and more generally to improve and optimize our Site (for example, by generating analytics about how our customers browse and interact with the Site, and to assess the success of our marketing and advertising campaigns).
+          </p>
+        </section>
+
+        <section class="mb-5">
+          <h4 class="mb-3">SHARING YOUR PERSONAL INFORMATION</h4>
+          <p>We share your Personal Information with third parties to help us use your Personal Information, as described above.  For example, we use Wix to power our online store--you can read more about how Wix uses your Personal Information here:  <a href="https://www.wix.com/about/privacy" target="_blank">https://www.wix.com/about/privacy</a>.  We also use Google Analytics to help us understand how our customers use the Site--you can read more about how Google uses your Personal Information here:  https://www.google.com/intl/en/policies/privacy/.  You can also opt-out of Google Analytics here: <a href="https://tools.google.com/dlpage/gaoptout" target="_blank">https://tools.google.com/dlpage/gaoptout</a>.</p>
+          <br>
+          <p>Finally, we may also share your Personal Information to comply with applicable laws and regulations, to respond to a subpoena, search warrant or other lawful request for information we receive, or to otherwise protect our rights.</p>
+          <br>
+          <p>As described above, we use your Personal Information to provide you with targeted advertisements or marketing communications we believe may be of interest to you.  For more information about how targeted advertising works, you can visit the Network Advertising Initiative’s (“NAI”) educational page at <a href="http://www.networkadvertising.org/understanding-online-advertising/how-does-it-work" target="_blank">http://www.networkadvertising.org/understanding-online-advertising/how-does-it-work</a>.</p>
+          <br>
+          <p>You can opt out of targeted advertising by:</p>
+          <ul>
+            <li><a href="https://www.facebook.com/settings/?tab=ads" target="_blank">FACEBOOK - https://www.facebook.com/settings/?tab=ads</a></li>
+            <li><a href="https://www.google.com/settings/ads/anonymous" target="_blank">GOOGLE - https://www.google.com/settings/ads/anonymous</a></li>
+            <li><a href="https://advertise.bingads.microsoft.com/en-us/resources/policies/personalized-ads" target="_blank">BING - https://advertise.bingads.microsoft.com/en-us/resources/policies/personalized-ads</a></li>
+          </ul>
+          <br>
+          <p>Additionally, you can opt out of some of these services by visiting the Digital Advertising Alliance’s opt-out portal at: <a href="http://optout.aboutads.info/" target="_blank">http://optout.aboutads.info/</a>.</p>
+        </section>
+
+        <section class="mb-5">
+          <h4 class="mb-3">DO NOT TRACK</h4>
+          <p>Please note that we do not alter our Site’s data collection and use practices when we see a Do Not Track signal from your browser.</p>
+          <br>
+          <p>You have the right to access personal information we hold about you and to ask that your personal information be corrected, updated, or deleted. If you would like to exercise this right, please contact us through the contact information below.</p>
+          <br>
+          <p>Additionally, we note that we are processing your information in order to fulfill contracts we might have with you (for example if you make an order through the Site), or otherwise to pursue our legitimate business interests listed above.</p>
+        </section>
+
+        <section class="mb-5">
+          <h4 class="mb-3">DATA RETENTION</h4>
+          <p>When you place an order through the Site, we will maintain your Order Information for our records unless and until you ask us to delete this information.</p>
+        </section>
+
+        <section class="mb-5">
+          <h4 class="mb-3">MINORS</h4>
+          <p>The Site is not intended for individuals under the age of 18 years old.</p>
+        </section>
+
+        <section class="mb-5">
+          <h4 class="mb-3">CHANGES</h4>
+          <p>We may update this privacy policy from time to time in order to reflect, for example, changes to our practices or for other operational, legal or regulatory reasons.</p>
+        </section>
+
+        <section class="mb-5">
+          <h4 class="mb-3">CONTACT US</h4>
+          <p>For more information about our privacy practices, if you have questions, or if you would like to make a complaint, please contact us by e-mail at <a href="mailto:linden.teak.furniture@gmail.com">linden.teak.furniture@gmail.com</a> or by mail using the details provided below:</p>
+          <br>
+          <p>Pitimini,  Quezon City, 00, 1008, Philippines</p>
+          <p>You can also call us at <a href="tel:6389218037">(+63) 89218037</a> / <a href="tel:6374137278">(+63) 74137278</a></p>
+        </section>
+      </div>
+    </div>
+    <div class="container pt-4 pb-4">
+      <div class="row">
+        <div class="col-lg-6 text-center text-lg-left">
+          <img src="<?= base_url('image/secure-payment.png'); ?>" class="img-fluid" alt="Responsive image">
+          <ul class="list-unstyled text-small pl-2 mt-2" style="line-height: 2">
+            <li><a class="text-muted" href="<?= base_url('privacy-policy') ?>">Privacy policy</a></li>
+            <li><a class="text-muted" href="https://media.wix.com/ugd/f034d8_473be673e5b74671acf13de694b90e9e.pdf">Terms & Conditions</a></li>
+          </ul>
+        </div>
+        <div class="col-lg-6 text-center text-lg-right">
+          <hr class="d-lg-none">
+          <span>Designed & built by <a href="http://thinkdigitalph.com/" target="_blank">Think Digital PH</a></span>
+        </div>
+      </div>
+    </div>
+    <!-- CONTAINER -->
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script> let base_url = "<?= base_url(); ?>"</script>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://getbootstrap.com/docs/4.0/assets/js/vendor/popper.min.js"></script>
+    <script src="https://getbootstrap.com/docs/4.0/dist/js/bootstrap.min.js"></script>
+    <script src="https://getbootstrap.com/docs/4.0/assets/js/vendor/holder.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="<?= base_url('js/commons.js?v=').strtotime('now'); ?>"></script>
+</body></html>
